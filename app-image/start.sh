563a85adec1c258962500c5cd0178359ce202ed7
gnios <eugenio00@gmail.com>
2017-03-30 08:40:30 -0300

    teste

--- app-image/start.sh
+++ app-image/start.sh
@@ -5,8 +5,6 @@ echo build starting nginx config
 
 
 echo replacing ___my.example.com___/$MY_DOMAIN_NAME
-echo replacing ___LETSENCRYPT_IP___/$LETSENCRYPT_PORT_80_TCP_ADDR
-echo replacing ___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80_TCP_PORT
 echo replacing ___APPLICATION_IP___/$APP_PORT_80_TCP_ADDR
 echo replacing ___APPLICATION_PORT___/$APP_PORT_80_TCP_PORT
 
@@ -15,8 +13,6 @@ sed -i "s/___my.example.com___/$MY_DOMAIN_NAME/g" /etc/nginx/nginx.conf
 # Add your app's container IP and port into config
 sed -i "s/___APPLICATION_IP___/$APP_PORT_80_TCP_ADDR/g" /etc/nginx/nginx.conf
 sed -i "s/___APPLICATION_PORT___/$APP_PORT_80_TCP_PORT/g" /etc/nginx/nginx.conf
-sed -i "s/___LETSENCRYPT_IP___/$LETSENCRYPT_PORT_80_TCP_ADDR/g" /etc/nginx/nginx.conf
-sed -i "s/___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80_TCP_PORT/g" /etc/nginx/nginx.conf
 
 cat /etc/nginx/nginx.conf
 echo .
